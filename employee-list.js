58ccf9573210c6e0cc1954ec175df591fb365757
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2024-07-02 14:10:32 +0200

    generation 500 employees

--- employee-list.js
+++ employee-list.js
@@ -1,5 +1,31 @@
 $(function() {
-    const employees = JSON.parse(localStorage.getItem('employees'));
+
+    const generateObjects = () => {
+        const objects = [];
+        let startDate = new Date('9/1/1978')
+        let dateOfBirth = new Date('7/17/1976')
+        for (let i = 0; i < 500; i++) {
+            objects.push({
+                firstName: 'firstName '+i,
+                lastName: 'lastName '+i,
+                startDate: startDate?.toDateString(),
+                department: 'department '+i,
+                dateOfBirth: dateOfBirth?.toDateString(),
+                street: 'street '+i,
+                city: 'city' +i,
+                state: 'state '+i,
+                zipCode: 'zipCode '+i,
+            });
+
+            startDate.setDate(startDate.getDate() + 1);
+
+            dateOfBirth.setDate(dateOfBirth.getDate() + 1);
+        }
+        return objects;
+    };
+
+    let employees = JSON.parse(localStorage.getItem('employees'));
+    employees = employees.concat(generateObjects());
 
     $('#employee-table').DataTable({
         data: employees,
